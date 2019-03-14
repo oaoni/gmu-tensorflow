146dc460b8a376cfb4cbcfd0347aa9289c0c9fe7
Home <oa.oni7@gmail.com>
2019-03-14 13:43:05 -0400

    add gmu model file

---
+++ dgmu/models/gmu.py
@@ -0,0 +1,9 @@
+"""Gated multimodal unit based fusion and classification using Tensorflow """
+
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+from dgmu.core import 
