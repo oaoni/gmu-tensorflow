9b1568b925cd1084b74cbd57c9c81ee9c2578ee8
Home <oa.oni7@gmail.com>
2019-03-15 19:15:29 -0400

    auto import all file classes and functions

--- dgmu/core/__init__.py
+++ dgmu/core/__init__.py
@@ -1,1 +1,9 @@
 """dGMU core package"""
+
+from __future__ import absolute_import
+
+from .utils import *
+from .layers import *
+from .trainers import *
+from .model import *
+from .supervised_model import *
