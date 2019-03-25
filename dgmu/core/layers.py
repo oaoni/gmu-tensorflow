de3d63470a280b819c744bfeb44e1ba93fd14bc5
oaoni <oa.oni7@gmail.com>
2019-03-25 15:49:15 -0400

    refactor to add act_func to linear() static method

--- dgmu/core/layers.py
+++ dgmu/core/layers.py
@@ -5,31 +5,31 @@
 #Compare with the methodologies for activation functions
 ##################################################################
 
-import tensorflow
+import tensorflow as tf
 
 class Layers(object):
     """Implementation of NN layers"""
 
     @staticmethod
-    def activate(self, linear, act_type):
+    def activate(linear, name):
         """Implements desired non linearity
 
         :param linear: tf.Tensor
         :param act_type: string, activation type ['sigmoid', softmax, linear, tanh, relu]
         """
         if name == 'sigmoid':
-            return tf.nn.sigmoid(linear, name='sigmoid activation')
+            return tf.nn.sigmoid(linear, name='sigmoid-activation')
         elif name == 'softmax':
-            return tf.nn.softmax(linear, name='softmax activation')
+            return tf.nn.softmax(linear, name='softmax-activation')
         elif name == 'linear':
             return linear
         elif name == 'tanh':
-            return tf.nn.tanh(linear, name='tanh activation')
+            return tf.nn.tanh(linear, name='tanh-activation')
         elif name == 'relu':
-            return tf.nn.relu(linear, name='relu activation')
+            return tf.nn.relu(linear, name='relu-activation')
 
     @staticmethod
-    def linear(input_layer, hidden_dim, dropout_rate, name = 'linear'):
+    def linear(input_layer, hidden_dim, dropout_rate = 0, act_func = 'relu', summary = True, name = 'linear'):
         """Creates fully connected layer
 
         :param input_layer: tf.Tensor (n_samples, n_features), input layer
@@ -43,12 +43,17 @@ class Layers(object):
 
         with tf.name_scope(name):
             input_dim = input_layer.get_shape()[1].value
-            W = tf.Variable(tf.truncated_normal([input_dim, hidden_dim]))
-            b = tf.Variable(tf.truncated_normal([hidden_dim]))
-            _dropout = tf.nn.dropout(input_layer, rate = dropout_rate) #input dropout
+            W = tf.Variable(tf.truncated_normal([input_dim, hidden_dim]), name='W')
+            b = tf.Variable(tf.truncated_normal([hidden_dim]), name='b')
+            _dropout = tf.nn.dropout(input_layer, keep_prob = 1-dropout_rate) #input dropout
             hidden_layer = tf.add(tf.matmul(_dropout, W), b)
+            activations = Layers.activate(hidden_layer, act_func)
+            if summary:
+                tf.summary.histogram('weights', W)
+                tf.summary.histogram('biases', b)
+                tf.summary.histogram('activations', activations)
 
-            return (hidden_layer, W, b)
+            return (activations, W, b)
 
     @staticmethod
     def maxout(input_layer, hidden_dim, pool_depth, dropout_rate, name = 'maxout'):
@@ -69,35 +74,35 @@ class Layers(object):
             W = tf.Variable(tf.truncated_normal([input_dim,(hidden_dim*pool_depth)]))
             b = tf.Variable(tf.truncated_normal([(hidden_dim*pool_depth)]))
             z = tf.add(tf.matmul(input_layer, W), b)
-            _dropout = tf.nn.dropout(z, rate = dropout_rate) #hidden dropout
+            _dropout = tf.nn.dropout(z, keep_prob = 1-dropout_rate) #hidden dropout
             _maxout = tf.reduce_max(tf.reshape(_dropout, [-1, hidden_dim, pool_depth]), axis=2)
             output_layer = tf.reshape(_maxout, [-1, hidden_dim])
 
             return (output_layer, W, b)
 
-        @staticmethod
-        def regularize(variables, reg_type, beta, name = 'regularization'):
-            #Sampled from yadlt/core/layers.py
-            """Computes regularization tensor
+    @staticmethod
+    def regularize(variables, reg_type, beta, name = 'regularization'):
+        #Sampled from yadlt/core/layers.py
+        """Computes regularization tensor
 
-            :param variables: list of tf.Variable
-            :param reg_type: string, type of regularization ['none','l1','l2']
-            :param beta: float, regularization parameter
-            :param name: string, name of scope
-            :return tf.Tensor, regularization operation tensor
-            """
+        :param variables: list of tf.Variable
+        :param reg_type: string, type of regularization ['none','l1','l2']
+        :param beta: float, regularization parameter
+        :param name: string, name of scope
+        :return tf.Tensor, regularization operation tensor
+        """
 
-            with tf.name_scope(name):
-                if reg_type != 'none':
-                    regs = tf.constant(0.0)
-                    for v in variables:
-                        if reg_type == 'l2':
-                            regs = tf.add(regs, tf.nn.l2_loss(v))
-                        elif reg_type == 'l1':
-                            regs = tf.add(regs, tf.reduce_sum(tf.abs(v)))
+        with tf.name_scope(name):
+            if reg_type != 'none':
+                regs = tf.constant(0.0)
+                for v in variables:
+                    if reg_type == 'l2':
+                        regs = tf.add(regs, tf.nn.l2_loss(v))
+                    elif reg_type == 'l1':
+                        regs = tf.add(regs, tf.reduce_sum(tf.abs(v)))
 
-                    return tf.multiply(beta, regs)
+                return tf.multiply(beta, regs)
 
-                else:
+            else:
 
-                    return None
+                return None
